cbd6c14cd2caa4353a1a449ea5aa5716ce55748b
datdevcoffee <datdevcoffee@gmail.com>
2025-09-08 18:25:37 +0700

    Update server.js
    const jsonServer = require('json-server');
    const server = jsonServer.create();
    const router = jsonServer.router('db.json');
    const middlewares = jsonServer.defaults({ static: './' });
    
    server.use(middlewares);
    server.use(router);
    
    module.exports = server;

--- api/server.js
+++ api/server.js
@@ -1,13 +1,9 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
+const middlewares = jsonServer.defaults({ static: './' });
 
 server.use(middlewares);
 server.use(router);
 
-const port = process.env.PORT || 3000;
-
-server.listen(port, () => {
-  console.log(`JSON Server is running on port ${port}`);
-});
+module.exports = server;
